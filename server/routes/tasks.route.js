e88f2d0f5d5e972200e6c6abf56bb82518caaf48
Travis Lang <tlang50@icloud.com>
2018-11-26 17:49:21 -0600

    fix typo

--- server/routes/tasks.route.js
+++ server/routes/tasks.route.js
@@ -1,7 +1,6 @@
 const express = require( 'express' );
 const router = express.Router( );
 const bodyParser = require('body-parser');
-const pg = require( 'pg' );
 const pool = require('../modules/pool.module.js');
 
 // // DB CONNECTION
